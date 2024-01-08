b6b6eff5f5fe7fb5de59362696cfd92e2c5136e8
fahimakm <115616469+fahimakm@users.noreply.github.com>
2024-01-08 22:49:10 +0530

    changes in dynamic project

--- dynamicproject/dynamicapp/models.py
+++ dynamicproject/dynamicapp/models.py
@@ -8,3 +8,10 @@ class place(models.Model):
 
     def __str__(self):
         return self.name
+class team(models.Model):
+    name = models.CharField(max_length=250)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.name
--- dynamicproject/dynamicapp/views.py
+++ dynamicproject/dynamicapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import place
+from .models import team
 # Create your views here.
 def demo(request):
     obj=place.objects.all()
-    return render(request,'index.html',{'result':obj})
+    team_obj=team.objects.all()
+    return render(request,'index.html',{'result':obj,'team':team_obj})
